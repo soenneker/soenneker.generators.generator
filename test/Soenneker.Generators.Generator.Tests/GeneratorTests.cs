2b06dea67c176b0e84b62e051d7ab4a11e7f8a36
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:09 -0500

    TUnit conversion

--- test/Soenneker.Generators.Generator.Tests/GeneratorTests.cs
+++ test/Soenneker.Generators.Generator.Tests/GeneratorTests.cs
@@ -1,16 +1,15 @@
-﻿using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Generators.Generator.Tests;
 
-[Collection("Collection")]
-public class GeneratorTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class GeneratorTests : HostedUnitTest
 {
-    public GeneratorTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public GeneratorTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
