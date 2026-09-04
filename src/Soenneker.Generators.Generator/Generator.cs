d6e0603db66a875527eb68120f96fd12af49c6ef
soenneker <jake@soenneker.com>
2026-09-03 22:01:21 -0500

    Add interface inheritdoc references

--- src/Soenneker.Generators.Generator/Generator.cs
+++ src/Soenneker.Generators.Generator/Generator.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 
 namespace Soenneker.Generators.Generator;
 
+/// <inheritdoc cref="IGenerator" />
 public abstract class Generator : IGenerator
 {
     public abstract ValueTask Generate(CancellationToken cancellationToken = default);
